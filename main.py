a8083ea107e86425de0f5bb56642b007544eb046
vyle4378 <vyle4378@gmail.com>
2026-04-26 22:50:42 -0700

    increased max_tokens

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ async def format_problems(input: GeneratedProblems):
     response = client.messages.create(
         model=MODEL,
         system=FORMAT_PROMPT,
-        max_tokens=8000,
+        max_tokens=16000,
         messages=[
             {
                 "role": "user",
